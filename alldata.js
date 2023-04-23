5729e600ac9bb79ae570dea20d4337ebfb79d462
SamanthaJWheeler <115271437+SamanthaJWheeler@users.noreply.github.com>
2023-04-23 12:57:37 -0400

    Update alldata.js
    all data functionality

--- alldata.js
+++ alldata.js
@@ -1,9 +1,31 @@
 function AllData(){
   const ctx = React.useContext(UserContext);
-  return (
-    <>
-    <h5>All Data in Store</h5>
-    {JSON.stringify(ctx)}<br/>
-    </>
+  const usersData = ctx.users;
+
+  function createCard(user) {
+    const { name, email, password, balance} = user;
+    return (
+      <center><Card 
+        header = "All Data"
+        bgcolor="primary"
+        body={
+          <>
+            <p className="card-text">Name: {name}</p>
+            <p className="card-text">Email: {email}</p>
+            <p className="card-text">Password: {password}</p>
+            <p className="card-text">Balance: ${balance}</p>
+          </>
+        }
+        />
+      </center>
+    );
+  }
+
+  const cards = usersData.map(createCard);
+
+  return(
+    <div>
+      {cards}
+    </div>
   );
 }
