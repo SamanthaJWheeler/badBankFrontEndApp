38af35000a7f3fe8a12cffba0258dd7e41f9a6ef
SamanthaJWheeler <115271437+SamanthaJWheeler@users.noreply.github.com>
2023-04-30 19:51:24 -0400

    yataaa react spa stand alone !

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-function Spa() {
-  return (
-    <HashRouter>
-      <NavBar/>
-      <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
-        <div className="container" style={{padding: "20px"}}>
-          <Route path="/" exact component={Home} />
-          <Route path="/CreateAccount/" component={CreateAccount} />
-          <Route path="/deposit/" component={Deposit} />
-          <Route path="/withdraw/" component={Withdraw} />
-          <Route path="/alldata/" component={AllData} />
-        </div>
-      </UserContext.Provider>      
-    </HashRouter>
-  );
-}
+import React from "react";
+import reactDom from "react-dom";
+import App from "./src/app";
 
-ReactDOM.render(
-  <Spa/>,
-  document.getElementById('root')
-);
+reactDom.render(<App />, document.getElementById("root"));
---
+++ src/app.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+// const App = () => {
+  
+//   // return (
+//   //   <HashRouter>
+//   //     <NavBar/>
+//   //     <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
+//   //       <div className="container" style={{padding: "20px"}}>
+//   //         <Route path="/" exact component={Home} />
+//   //         <Route path="/CreateAccount/" component={CreateAccount} />
+//   //         <Route path="/deposit/" component={Deposit} />
+//   //         <Route path="/withdraw/" component={Withdraw} />
+//   //         <Route path="/alldata/" component={AllData} />
+//   //       </div>
+//   //     </UserContext.Provider>      
+//   //   </HashRouter>
+//   // );
+// }
+
+//ReactDOM.render(
+//  <Spa/>,
+//  document.getElementById('root')
+//);
+
+const App = () =>{
+  return (
+      <h1>
+          Welcome to React App thats build using Webpack and Babel separately
+      </h1>
+  )
+}
+
+export default App
+
+
